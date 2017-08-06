ac42168ef7a83ec6e64a335ee266e74b9589c112
Andrew Andrianov <andrew@ncrmnt.org>
2017-08-06 15:13:07 +0300

    Add firmware from UMI-X2
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- scripts/mt6620_start_wireless.sh
+++ scripts/mt6620_start_wireless.sh
@@ -2,7 +2,7 @@
 
 # Copy-paste from android init
 
-if [ ! /dev/wmtWifi ]; then
+if [ ! -e /dev/wmtWifi ]; then
     mknod /dev/stpwmt c 190 0
     mknod /dev/stpgps c 191 0
     mknod /dev/stpbt  c 192 0
